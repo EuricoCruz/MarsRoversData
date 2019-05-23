4299829801565b29c0947335e5ef1f8e9c7b15f3
EuricoCruz <euricoscruz@hotmail.com>
2019-05-23 18:57:56 -0300

    first commit

---
+++ javascripts/app.js
@@ -0,0 +1,66 @@
+// Rover Object Goes Here
+// ======================
+
+// ======================
+
+var rover = {
+  direction: "N",
+  x = 0;
+  y = 0;
+}
+
+function turnLeft(rover){
+  console.log("turnLeft was called!");
+  switch(rover.direction) {
+  case "N": 
+  rover.direction = "w"
+  break;
+  case "E": 
+  rover.direction = "N"
+  break;
+  case "S": 
+  rover.direction = "E"
+  break;
+  case "W":
+  rover.direction = "S"
+  break;
+  }
+}
+
+function turnRight(rover){
+  console.log("turnRight was called!");
+  switch(rover.direction) {
+    case "N": 
+    rover.direction = "E"
+    break;
+    case "E": 
+    rover.direction = "S"
+    break;
+    case "S": 
+    rover.direction = "W"
+    break;
+    case "W":
+    rover.direction = "N"
+    break;
+    
+  }
+}
+
+function moveForward(rover){
+  console.log("moveForward was called")
+}
+
+var grid = [
+  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+]
+
+
